65d58fb36c77be90751e156b2590c8aff86fe6ce
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-24 10:04:17 +0200

    Initialized project

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+
+!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+</head>
+<body>
+    
+</body>
+</html>
