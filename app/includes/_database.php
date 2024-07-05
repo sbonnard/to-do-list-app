a669ae2462b6ee8eecd03fff56121f4b990ea834
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-07-05 10:33:42 +0200

    .env done, install composer dependency and connected my database in a way more secured way

--- app/includes/_database.php
+++ app/includes/_database.php
@@ -1,11 +1,17 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PWD']);
 
 // CONNECTION 
 try {
     $dbCo = new PDO(
-        'mysql:host=db;dbname=jotit_doit;charset=utf8',
-        'app-crud',
-        'dwwm2024'
+        'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8',
+        $_ENV['DB_USER'],
+        $_ENV['DB_PWD']
     );
     $dbCo->setAttribute(
         PDO::ATTR_DEFAULT_FETCH_MODE,
